a3d18880576e599ca1fda58fc86dd91528605f99
Yves Mauricio <yvesmaus@gmail.com>
2026-04-22 17:33:09 -0300

    Nova Tela Extrato

--- public/receita_atualizado (2)/src/composables/useSwipe.js
+++
@@ -1,9 +0,0 @@
-import { ref } from 'vue'
-
-// Singleton: só um SwipeRow aberto por vez em toda a aplicação
-const openSwipeId = ref(null)
-
-export function useSwipe() {
-  function closeAll() { openSwipeId.value = null }
-  return { openSwipeId, closeAll }
-}
